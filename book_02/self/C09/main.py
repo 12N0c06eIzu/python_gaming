77916175084c1ce3fab3f9c605ad7a8dd9a333be
いず <ritu6811@gmail.com>
2022-08-21 21:27:38 +0900

    commit

--- book_02/self/C09/main.py
+++ book_02/self/C09/main.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 import random
-# import os
-# print(os.getcwd())
 
 class Window():
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        self.index = 0
+        self.timer = 0
+        self.score = 0
+        self.tsugi = 0
 
 class Cursor():
     def __init__(self, icon):
@@ -21,18 +23,11 @@ class Mouse():
         self.c = 0
 
 # 猫ピースの配置
-neko = [
-    [1, 0, 0, 0, 0, 0, 7, 7],
-    [0, 2, 0, 0, 0, 0, 7, 7],
-    [0, 0, 3, 0, 0, 0, 7, 7],
-    [0, 0, 0, 4, 0, 0, 7, 7],
-    [0, 0, 0, 0, 5, 0, 7, 7],
-    [0, 0, 0, 0, 0, 6, 7, 7],
-    [0, 0, 0, 0, 0, 0, 7, 7],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 2, 3, 4, 5, 6],
-]
+neko = []
+check = []
+for i in range(10):
+    neko.append([0, 0, 0, 0, 0, 0, 0, 0])
+    check.append([0, 0, 0, 0, 0, 0, 0, 0])
 
 # 猫ピースの画像を表示する。
 def draw_neko():
@@ -40,6 +35,39 @@ def draw_neko():
         for x in range(8):
             if neko[y][x] > 0:
                 cvs.create_image(x*72+60, y*72+60, image=img_neko[neko[y][x]], tag="NEKO")
+
+def check_neko():
+    for y in range(10):
+        for x in range(8):
+            check[y][x] = neko[y][x]
+
+    for y in range(1, 9):
+        for x in range(8):
+            if check[y][x] > 0:
+                if check[y - 1][x] == check[y][x] and check[y + 1][x] == check[y][x]:
+                    neko[y - 1][x] = 7
+                    neko[y][x] = 7
+                    neko[y + 1][x] = 7
+
+    for y in range(10):
+        for x in range(1, 7):
+            if check[y][x] > 0:
+                if check[y][x - 1] == check[y][x] and check[y][x + 1] == check[y][x]:
+                    neko[y][x - 1] = 7
+                    neko[y][x] = 7
+                    neko[y][x + 1] = 7
+
+        for y in range(1, 9):
+            for x in range(1, 7):
+                if check[y][x] > 0:
+                    if check[y - 1][x - 1] == check[y][x] and check[y + 1][x + 1] == check[y][x]:
+                        neko[y - 1][x - 1] = 7
+                        neko[y][x] = 7
+                        neko[y + 1][x + 1] = 7
+                    if check[y + 1][x - 1] == check[y][x] and check[y -1 ][x + 1] == check[y][x]:
+                        neko[y + 1][x - 1] = 7
+                        neko[y][x] = 7
+                        neko[y - 1][x + 1] = 7
 
 # 猫ピースの落下アルゴリズム
 def drop_neko():
@@ -58,6 +86,10 @@ def mouse_press(e):
 
 def game():
     drop_neko()
+    # 風船押したら判定する。
+    if 660 <= m.x and m.y < 840 and 100 <= m.y and m.y < 160 and m.c == 1:
+        m.c = 0
+        check_neko()
     # 盤上をマウスカーソルが飛び出てしまわないようにする。
     if 24 <= m.x and m.x < 24+72*8 and 24 <= m.y and m.y < 24+72*10:
         cs.x = int((m.x - 24) / 72)
@@ -83,13 +115,13 @@ root.title("マウス入力")
 root.resizable(False, False)
 root.bind("<Motion>", mouse_move)
 root.bind("<ButtonPress>", mouse_press)
-
 cvs = tk.Canvas(root, width=window.width, height=window.height)
 cvs.pack()
 
 bg = tk.PhotoImage(file="./assets/neko_bg.png")
 cursor = tk.PhotoImage(file=cs.icon)
 
+# 猫のイメージを保存する。
 img_neko = [
     None,
     tk.PhotoImage(file="./peace/1.png"),
@@ -102,6 +134,8 @@ img_neko = [
 ]
 
 cvs.create_image(456, 384, image=bg)
+cvs.create_rectangle(660, 100, 840, 160, fill="white")
+cvs.create_text(750, 130, text="テスト", fill="red", font=("Times New Roman", 30))
 
 game()
 root.mainloop()
